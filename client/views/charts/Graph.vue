99ec71d3d63ba856f94dfff9c81283743043b282
Daniel Beal <git@danb.email>
2017-09-23 11:54:36 -0700

    Begin working on charting for graphs

---
+++ client/views/charts/Graph.vue
@@ -0,0 +1,115 @@
+<template>
+<div ref="chart"></div>
+</template>
+
+<style lang="scss" scoped>
+svg {
+    width: 100%;
+    min-height: 200px;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+import _ from 'lodash';
+import * as c3 from 'c3';
+import * as d3 from 'd3';
+
+@Component({
+    props: {
+        x: {
+            type: String
+        },
+        xLabel: {
+            type: String,
+            default: ''
+        },
+        yLabel: {
+            type: String,
+            default: ''
+        },
+        yFormat: {
+            type: String,
+            default: null
+        },
+        points: {
+            type: Array,
+            required: true
+        }
+    },
+    watch: {
+        points: 'updatePoints'
+    }
+})
+export default class Graph extends Vue {
+
+    chart: c3.ChartAPI;
+
+    x: string;
+    points: (string|number|boolean)[][];
+
+    xLabel: string;
+    yLabel: string;
+
+    yFormat: string;
+
+
+    get chartX() {
+        return this.x;
+    }
+
+    get chartXLabel() {
+        return this.xLabel;
+    }
+
+    get chartYLabel() {
+        return this.yLabel;
+    }
+
+    get chartYFormat() {
+        return d3.format(this.yFormat);
+    }
+
+    mounted() {
+        this.chart = c3.generate({
+            bindto: <HTMLDivElement>this.$refs.chart,
+            data: {
+                x: this.chartX,
+                columns: this.points,
+
+            },
+            axis: {
+                x: {
+                    label: {
+                        text: this.chartXLabel,
+                        position: 'outer-middle'
+                    }
+                },
+                y: {
+                    label: {
+                        text: this.chartYLabel,
+                        position: 'outer-middle'
+                    },
+                    tick: {
+                        format: this.chartYFormat
+                    }
+                }
+            }
+        });
+    }
+
+    updatePoints(oldPoints: (string|number|boolean)[][], newPoints: (string|number|boolean)[][]) {
+        // figure out where the changes happened
+        // first remove nonexistant ones, then add new ones
+        const removed = _.map(_.difference(oldPoints, newPoints), _.head);
+        const added = _.difference(newPoints, oldPoints);
+
+        this.chart.load({
+            columns: added,
+            unload: <string[]>removed
+        });
+    }
+}
+</script>
---
+++ client/views/charts/TimeSeries.vue
@@ -0,0 +1,56 @@
+<template>
+<svg ref="diag">
+    <g class="lines">
+
+    </g>
+    <g class="nodes">
+
+    </g>
+</svg>
+</template>
+
+<style lang="scss" scoped>
+svg {
+    width: 100%;
+    min-height: 200px;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+import _ from 'lodash';
+import * as c3 from 'c3';
+import {u256, ShardInfo} from "../../../lib/primatives";
+
+@Component({
+    props: {
+        mode: {
+            type: String,
+            default: 'full'
+        },
+        startTime: {
+            type: Number,
+            require: true
+        },
+
+        points: {
+            type: Array,
+            required: true
+        }
+    }
+})
+export default class TimeSeries extends Vue {
+    mode: 'full' | 'time' | 'date';
+    startTime: number;
+    points: number[][];
+
+    mounted() {
+        const pointsMaxLength = _.max(_.map(this.points, 'length'));
+
+        // generate dates
+        
+    }
+}
+</script>
