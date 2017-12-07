e6f5a9a1e00973387da884c36bfb508bb5976288
Daniel Beal <git@danb.email>
2017-12-07 12:06:38 -0800

    More fixes, now show I/O so we can prove my fail

--- client/views/Header.vue
+++ client/views/Header.vue
@@ -2,8 +2,8 @@
 <header class="header">
 <div class="infobar">
     <div class="kv">
-        <span class="key"># Nodes:</span>
-        <span class="value">{{stats.connected_peers}}</span>
+        <span class="key">I/O:</span>
+        <span class="value">{{stats.rx + stats.tx}}</span>
     </div>
     <div class="kv">
         <span class="key">Root Height:</span>
--- lib/api/stats.ts
+++ lib/api/stats.ts
@@ -8,7 +8,7 @@ router.get('/net', async (_, res) => {
     try {
         let stats = await rpc().request('get_net_stats', []);
     
-        res.json(stats);
+        res.json(stats.result);
     } catch(err) {
         console.error(err);
         res.status(502).end('Service Unavailable');
